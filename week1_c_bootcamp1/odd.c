8d7a2390892848be80d1374a3a7135634ef17dbf
Shaan Shaikh <146674404+Shaan-07@users.noreply.github.com>
2023-10-12 09:45:09 +0000

    -

---
+++ week1_c_bootcamp1/odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+    int main(){
+
+        int a;
+        // int x = 10; //x is the first x odd numbers
+        // int odd = x * 2;
+
+        // for(a = 1; a < odd; a++)
+        // {
+        //    printf("the odd numbers are")
+        // }
+        for(a = 0; a < 10; a++) 
+        {
+            printf("%d\n", a*2+1);// the maths formula are useful in c 
+        }
+    return 0;
+}
