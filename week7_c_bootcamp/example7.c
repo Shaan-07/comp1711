eaa2367ed37b65ca12fd8c4d74f58f6f87cd8d79
Shaan Shaikh <146674404+Shaan-07@users.noreply.github.com>
2023-11-13 10:53:33 +0000

    -

---
+++ week7_c_bootcamp/example7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	char date[20];
+	float BloodIron;
+} reading;
+int main(){
+//creating a single instance
+reading today;
+
+//array with 7 members to store a week worth of reading
+reading week[7];
+}
