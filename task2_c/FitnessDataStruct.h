a7d7467bb53dfa7e833a0f9956f4984dcd329b8f
Shaan Shaikh <146674404+Shaan-07@users.noreply.github.com>
2023-11-25 01:49:54 +0000

    -

--- task2_c/FitnessDataStruct.h
+++ task2_c/FitnessDataStruct.h
@@ -10,6 +10,12 @@
 // declaring this value so I can use it in different places
 #define buffer_size 100
 
+/**
+ * @brief Struct to hold each fitness record, contains the date as a string
+ *        and the steps as a integer.
+ *
+ */
+
 // Define an appropriate struct
 typedef struct {
 	char date[11];
@@ -17,7 +23,53 @@ typedef struct {
 	int steps;
 } FITNESS_DATA;
 
+/**
+ * @brief Adapted version of the tokeniseRecord function which you should now be familiar with - this one is adapted for this data file
+ *        as it has fewer outputs and gives you the steps as integer.
+ * @param input the line of the file to be split
+ * @param delimiter what character it should split on
+ * @param date the place where the date will be stored
+ * @param time the place where the time will be stored.
+ * @param steps the place where the steps will be stored
+ */
+
 // Helper function prototypes
 void tokeniseRecord(const char *input, const char *delimiter, char *date, char *time, char *steps);
+
+/**
+ * @brief Opens the file in the correct mode
+ *
+ * @param filename the name of the file to open
+ * @param mode the mode (r/w/a/r+/w+/a+)
+ * @return FILE* The file object to store the opened file in.
+ */
+FILE *open_file(char *filename, char *mode)
+{
+    // to do
+}
+
+/**
+ * @brief Reads the data from the input file into an array of structs
+ *
+ * @param inputFile the open file object
+ * @param dataArray the array of readings
+ * @return int Returns the number of readings from the file
+ */
+int read_file(FILE *inputFile, FITNESS_DATA *dataArray)
+{
+    // to do
+}
+
+/**
+ * @brief Checks that there was data for each part of each reading in the file
+ *
+ * @param dataArray The array of data from the file
+ * @param numReadings The number of readings in the array
+ * @return int Return 0 if there are no errors, 1 if you find an error.
+ */
+int record_checker(FITNESS_DATA *dataArray, int numReadings)
+{
+    // to do
+}
 
 #endif // FITNESS_DATA_STRUCT_H
--- task2_c/StepCounter_Final.c
+++ task2_c/StepCounter_Final.c
@@ -58,6 +58,7 @@ int main() {
 
     char choice;
     int counter = 0;
+    int mean = 0;
 
     FILE *input = fopen(filename, "r");
         if (!input)
@@ -83,23 +84,59 @@ int main() {
     }
     // Always close your file or c will chew your face off.
     fclose(input);
-                    
-    printf("Number of records in file: %i\n", counter);
 
-    for (int i = 0; i < counter; i++) {//here I am using i<3 in middle but if we use i<a, it prints all the records in file.
-        printf("%s/", record[i].date);
-        printf("%s/", record[i].time);
-        printf("%u\n", record[i].steps);
-    }
-   
+    //for (int i = 0; i < counter; i++) {//here I am using i<3 in middle but if we use i<a, it prints all the records in file.
+        //printf("%s/", record[i].date);
+        //printf("%s/", record[i].time);
+        //printf("%u\n", record[i].steps);
+    //}
+    while (1)
+    {
+
+
+        printf("A: Specify the filename to be imported\n");                       // BRONZE
+        printf("B: Display the total number of records in the file\n");           // BRONZE
+        printf("C: View your lowest blood iron level\n");                     // SILVER
+        printf("D: View your highest blood iron level\n");                    // SILVER
+        printf("E: View the blood iron levels for a specific month\n");       // SILVER/GOLD
+        printf("F: See some additional statistics about your iron levels\n"); // GOLD - see readme.md
+        printf("G: Generate a graph of your iron levels\n");                  // GOLD/PLATINUM - see readme.md
+        printf("Q: Exit the program\n");
+
+        // get the next character typed in and store in the 'choice'
+        choice = getchar();
+
+        // this gets rid of the newline character which the user will enter
+        // as otherwise this will stay in the stdin and be read next time
+        while (getchar() != '\n');
+
+
+        // switch statement to control the menu.
+        switch (choice)
+        {
+        // this allows for either capital or lower case
+        case 'A':
+        case 'a':
+
+            for (int i = 0; i < counter; i++)
+            {
+                printf("%s - steps: %u\n", record[i].date,record[i].steps);
+            }
+            break;
+
+        case 'B':
+        case 'b':
+
+            //for (int i = 0; i < counter; i++)
+            //{
+                //mean += record[i].steps;
+            //}
+            //mean /= counter;
+            //printf("Your average steps were %u\n", mean);
+            printf("Number of records in file: %i\n", counter);
+            break;
+        }
     return 0;
 }
-
-
-
-
-
-
-
-
+}
 
